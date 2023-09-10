6d6937edbc9375ae61d9c8639873d1e9d427645a
swim-ashwin <125240172+swim-ashwin@users.noreply.github.com>
2023-09-10 13:13:29 -0700

    Add Updated Code

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+class palindrome {
+    public static void main(String[] args){
+        
+    }
+    
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int n = s.length();
+
+        for (int i=0; i<n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        }
+
+        return true;
+    }
+}
