ab3f23669ec0f2760caceb45c7dc135c453b2505
swim-ashwin <125240172+swim-ashwin@users.noreply.github.com>
2024-02-07 16:51:09 -0800

    Create Prefix.java

---
+++ Prefix.java
@@ -0,0 +1,6 @@
+// - input a string array str
+// - check if array is empty or null -> return “ ”
+// - iterate through each index in the first string str[0]
+// - for each character at i, iterate through each string str[j]
+// - check if i is equal to the length of str[j] or if charAt(i) is != to character ch
+// - if true, return substring of str[0] from 0 to i
