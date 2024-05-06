7589f77f23d36f138dc88949b07d1453985da7cf
swim-ashwin <125240172+swim-ashwin@users.noreply.github.com>
2024-05-05 19:43:06 -0700

    Update String Length 1 Billion.java

--- String Length 1 Billion.java
+++ String Length 1 Billion.java
@@ -1,17 +1,6 @@
 public class billionString {
     /*
      * create a 2d array to add the zip codes in
-     * check if number at location is a digit by finding ascii value
-     * go from adress order and add the digits to the array in row 1
-     * check the list to see if there are 8 characters
-     * remove the first three and you are left with a zip code
-     * when moving onto the next address create a new row in the 2d array and repreat the populating process
-     * return the 2d array 
-     */
-    
-    //VERSION 2
-    /*
-     * create a 2d array to add the zip codes in
      * start at index 0 and incremement by 1 and add each digit/letter to row 1 until you reach a space
      * when the space is encountered add to row 1 as well
      * traverse the address until another number is encountered
@@ -20,4 +9,17 @@ public class billionString {
      * traverse the array from start until you reach the index with the space
      * return all the characters that come after it
      */
+     
+     public static void main(String[] args){
+     }
+
+     public String zipCode(String[] str){
+
+        for(int i = 0; i < str.length;i++){
+            if(i == null){
+                
+            }
+        }
+        return null;       
+     }
 }
